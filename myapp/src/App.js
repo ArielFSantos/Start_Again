125a163d95fbb1a1aece752f1e038de15d8fa6f7
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-06-18 15:33:15 -0300

    att
    volteeeei

---
+++ myapp/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './components/header/header';
+
+function App() {
+  return (
+   <Title/>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/header/header.js
@@ -0,0 +1,6 @@
+function Title (){
+    return(
+        <h1>Tatá</h1>
+    )
+}
+export default Title
